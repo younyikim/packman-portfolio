7fab7e548b6b8c5be7e9219c6054482b978ba0e9
younyikim <rlakuku1221@gmail.com>
2022-06-22 08:55:28 +0900

    Add loader and change about me desc

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import Card from './Card'
 import './index.scss'
@@ -12,30 +13,42 @@ const About = () => {
     }, 3000)
   }, [])
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-            idx={15}
-          />
-        </h1>
-        <p>
-          안녕하세요. 저는 이번 앱잼에 웹 파트 비활 OB로 참여하는 김연이입니다.
-        </p>
-        <p>
-          지난 기수의 앱잼은 좋은 사람들과 함께한 좋은 기억들 뿐이었습니다.
-          그래서 이번에도 비활 OB로 앱잼에 다시 한 번 참여하게 되었습니다. 이번
-          앱잼은 이전보다 더 많은 인원이 함께해 더 좋은 퀄리티의 서비스를 만들수
-          있을 것이라고 기대하고 있습니다. 그래서 이번 앱잼에서는 개인적인
-          측면에서 뿐만 아니라, 서비스적인 측면에서도 지난 앱잼에서는 하지
-          못했던 것들을 많이 시도해보고 싶습니다.
-        </p>
-        <p>다들 만나서 반갑습니다. 앞으로 잘 부탁드립니다!</p>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            안녕하세요. 저는 이번 앱잼에 웹 파트 비활 OB로 참여하는&nbsp;
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['김', '연', '이']}
+              idx={15}
+            />
+            입니다.
+          </p>
+          <p>
+            지난 기수의 앱잼은 좋은 사람들과 함께한 좋은 기억들뿐이었습니다.
+            앱잼을 통해 성장했고, 소중한 인연들을 만났습니다. 그래서 이번 30기
+            앱잼에 다시 한번 비활 OB로 참여하게 되었습니다. 이번에는 어떤 좋은
+            사람들과 멋진 서비스를 개발하게 될까 시작 전부터 많은 기대를 하고
+            있습니다. 최고의 서비스와 최고의 팀을 만들기 위해, 늘 사용자와 팀을
+            배려할 줄 아는 개발자가 되도록 노력하겠습니다.
+          </p>
+          <p>
+            새로 만나게 된 여러분, 다시 만난 여러분 모두 반갑습니다. 앞으로 잘
+            부탁드립니다!
+          </p>
+        </div>
+        <Card />
       </div>
-      <Card />
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
 import AnimatedLetters from '../AnimatedLetters'
@@ -32,35 +33,38 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="container home-page">
-      {/* Animate text */}
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
-        <h2>김연이 / Frontend Developer / SOPT 29th Web Part</h2>
-        <Link to="/packman-portfolio" className="flat-button">
-          CONTACT ME
-        </Link>
+    <>
+      <div className="container home-page">
+        {/* Animate text */}
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>김연이 / Frontend Developer / SOPT 29th Web Part</h2>
+          <Link to="/packman-portfolio" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
